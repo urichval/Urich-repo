be9711618086c2d585dc5698ec3654e7607c7707
urichval <valereouo@gmail.com>
2026-02-06 08:49:46 +0100

    Update DemoGithubApplication.java

--- src/main/java/com/example/demo_github/DemoGithubApplication.java
+++ src/main/java/com/example/demo_github/DemoGithubApplication.java
@@ -2,12 +2,19 @@ package com.example.demo_github;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class DemoGithubApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoGithubApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGithubApplication.class, args);
+    }
 
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello, World!";
+    }
 }
